21242baa74e8e9c45646e4a60cde96982941ae13
JBondeson19 <jpbond58@gmail.com>
2020-09-16 13:02:00 -0400

    "index setup"

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+get "/index", to: "coupon#index"
+
+
+
+
+
+
+
+
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
